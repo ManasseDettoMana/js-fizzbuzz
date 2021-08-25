42f54bc87f1e85419a842155723fd1ccc341f977
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-08-25 15:22:03 +0200

    esercizio finito

---
+++ script.js
@@ -0,0 +1,15 @@
+for(let i=1;i<=100;i++){
+
+    if(i%3==0){
+        console.log("Fizz");
+        continue;
+    }else if(i%5==0){
+        console.log("Buzz");
+        continue;
+    }
+    if(i%3==0 && i%5==0){
+        console.log("Fizz Buzz");
+        continue;
+    }
+    console.log(i);
+}
